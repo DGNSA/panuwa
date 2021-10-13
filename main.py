07a10ed6784cdb25459e12e2ab12bbb00367896c
Gimhana <92419296+DGNSA@users.noreply.github.com>
2021-10-13 19:47:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ def send_welcome(message):
     bot.reply_to(message,"හායි, මමයි ඔයාලගෙ පණුවා")
 
 @bot.message_handler(commands=["hello"])
-def send_message(message):
+def send_hello(message):
     bot.send_message(message,"මොන හොදින් ද අනෙ. ඇදිරිනීතිය නිසා කොලයක්වත් කන්න බැ ගනන් වැඩි")
 
 @bot.message_handler(commands=["help"])
-def send_message(message):
+def send_help(message):
     bot.send_message(message,"උදව්වක් විදිහට මට පුලුවන් ඔයාගෙ ගෙදර මල් ගස්වල කොල කන්න")
 
 @bot.message_handler(commands=["panuwa"])
-def send_message(message):
+def send_panuwa(message):
     bot.send_message(message,"ඒ මගෙ නම.ඇයි මගෙ නම පාඩම් කරනවාද")
 
 @bot.message_handler(commands=["command"])
-def send_message(message):
+def send_command(message):
     bot.send_message(message,"start,hello,help,panuwa,command")
 
 bot.polling()
